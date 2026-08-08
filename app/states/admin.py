3d7b2d16605d74ce47e55460c8d756ccf29ea8b0
azikorimskiy-afk <azikorimskiy@gmail.com>
2026-08-08 17:30:27 +0500

    Create admin.py

---
+++ app/states/admin.py
@@ -0,0 +1,10 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class AddProductState(StatesGroup):
+    waiting_product_id = State()
+    waiting_name = State()
+    waiting_price = State()
+    waiting_description = State()
+    waiting_image = State()
+    waiting_stock = State()
