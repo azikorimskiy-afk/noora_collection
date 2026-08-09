a7e18c3c5f539dd3f05b6a967a8625dc64a7cb2b
azikorimskiy-afk <azikorimskiy@gmail.com>
2026-08-09 15:30:02 +0500

    Update shop.py

--- app/handlers/shop.py
+++ app/handlers/shop.py
@@ -1,4 +1,4 @@
-```python
+
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
@@ -1046,4 +1046,4 @@ async def nothing_handler(
 ):
 
     await callback.answer()
-```
+
